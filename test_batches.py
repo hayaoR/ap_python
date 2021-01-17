16eef912737e3d26d1fa286518c83485744ec205
koki honda <kokihonda@am.ics.keio.ac.jp>
2021-01-17 21:43:53 +0900

    testを拡張

--- test_batches.py
+++ test_batches.py
@@ -1,9 +1,9 @@
-from model import Batch, Orderline
+from model import Batch, OrderLine
 from datetime import date
 
 def test_allocating_to_a_batch_reduces_the_avilable_quantity():
     batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
-    line = Orderline('order-ref', 'SMALL-TABLE', 2)
+    line = OrderLine('order-ref', 'SMALL-TABLE', 2)
 
     batch.allocate(line)
 
@@ -12,7 +12,7 @@ def test_allocating_to_a_batch_reduces_the_avilable_quantity():
 def make_batch_and_line(sku, batch_qty, line_qty):
     return (
         Batch("batch-001", sku, batch_qty, eta=date.today()),
-        Orderline("order-123", sku, line_qty)
+        OrderLine("order-123", sku, line_qty)
     )
 
 def test_can_allocate_if_available_greater_than_required():
@@ -29,7 +29,16 @@ def test_can_allocate_if_available_equal_to_required():
 
 def test_cannot_allocate_if_skus_do_not_match():
     batch = Batch("Batch-001", "UMCONFOTABLE CHAIR", 100, eta=None)
-    different_sku_line = Orderline("order-123", "EXPENSIVE-TOASTER", 10)
+    different_sku_line = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
     assert batch.can_allocate(different_sku_line) is False
 
+def tet_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
 
+def test_allocation_is_idempotent():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    batch.allocate(line)
+    batch.allocate(line)
+    assert batch.available_quantity == 18
